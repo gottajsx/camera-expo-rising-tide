c485cf76d53fe2126594c675580bc9d482ade07c
gottajsx <gottajsx@gmail.com>
2024-09-21 17:00:51 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useWindowDimensions, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera/legacy';
+import { useRef, useState } from 'react';
 
 export default function App() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [type, setType] = useState(CameraType.back)
+  const [flashMode, setFlashMode] = useState('off')
+  const cameraRef = useRef()
+
+  const {width} =  useWindowDimensions()
+  const height = Math.round((width*16) / 9)
+
+  if (!permission) {
+    return <View />
+  }
+
+  if (!permission.granted) {
+    return(
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>You need permission to access the camera</Text>
+        <Button onPress={requestPermission} title='grant permission' />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        type={type}
+        ratio='16:9'
+        flashMode={flashMode}
+        style={{ height: height}}
+        ref={cameraRef}
+        autoFocus={true}
+      >
+
+      </Camera>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    flex:1,
+    justifyContent: 'center'
+  }
 });
